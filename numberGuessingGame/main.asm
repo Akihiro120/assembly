ca0fc8b7ad8dc00d8961c7b688e62d2898e0e5ee
akihiro <kevin2006phan@gmail.com>
2024-03-17 02:43:03 +1100

    updated guessing game

--- numberGuessingGame/main.asm
+++ numberGuessingGame/main.asm
@@ -2,14 +2,19 @@ extern printf
 extern time
 extern srand
 extern rand
+extern scanf
 
 section .data
+    scan_format: db "%i", 0
     print_format: db "%i", 10, 0
-    range: dd 10
+
+    ; message
+    message_range: db "Enter a Range Limit: ", 0
 
 section .bss
     seed: resd 1
     random_number: resd 1
+    range: resb 8
 
 section .text
     global main
@@ -22,6 +27,7 @@ main:
     ; time
     xor rdi, rdi
     call time
+    add rax, rax
     mov [seed], rax
 
     ; srand
@@ -32,11 +38,24 @@ main:
     call rand
     mov [random_number], rax
 
-    ; limit the range to 0 - 1
+    ; display range message
+    mov rdi, message_range
+    mov rsi, 0
+    call printf
+    xor rax, rax
+
+    ; get range from user input
+    mov rdi, scan_format
+    mov rsi, range
+    call scanf
+    xor rax, rax
+
+    ; limit the range to 0 - Range
     xor rdx, rdx
     mov rax, [random_number]
     mov rdi, [range]
     div rdi
+    inc rdx
     mov [random_number], rdx
 
     ; print 
