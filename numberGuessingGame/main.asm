2c5602f01b0bfefab6154816bbf6e7d0b8804835
Akihiro <kevin2006phan@gmail.com>
2024-03-18 14:09:26 +1100

    finished number guessing game

--- numberGuessingGame/main.asm
+++ numberGuessingGame/main.asm
@@ -8,13 +8,18 @@ section .data
     scan_format: db "%i", 0
     print_format: db "%i", 10, 0
 
+	answer_message: db "Guess the number!!: ", 0
+	lose_message: db "You got the number wrong!!! it was: %i", 10, 0
+	win_message: db "You got the number right!!!", 10, 0
+
     ; message
     message_range: db "Enter a Range Limit: ", 0
 
 section .bss
-    seed: resd 1
-    random_number: resd 1
-    range: resb 8
+    seed: resq 1
+    random_number: resq 1
+    range: resq 1
+	answer: resq 1
 
 section .text
     global main
@@ -27,7 +32,9 @@ main:
     ; time
     xor rdi, rdi
     call time
-    add rax, rax
+	mov rdx, 0
+	mov rsi, 736467
+    mul rsi
     mov [seed], rax
 
     ; srand
@@ -58,17 +65,65 @@ main:
     inc rdx
     mov [random_number], rdx
 
-    ; print 
-    mov rdi, print_format
-    mov rsi, [random_number]
-    call printf
-    xor rax, rax
+	; get the users answer
+	; print
+	mov rdi, answer_message
+	mov rsi, 0
+	call printf
+	xor rax, rax
+
+	; scan
+	mov rdi, scan_format
+	mov rsi, answer
+	call scanf
+	xor rax, rax
 
     ; deallocate stack
     mov rsp, rbp
     pop rbp
 
+	; compare answer
+	mov rdi, [random_number]
+	mov rsi, [answer]
+	cmp rdi, rsi
+	je _win
+	jne _lose
+
     ; end
     mov rax, 60
     mov rdi, 0
     syscall
+
+_win:
+	; configure the stack
+	push rbp
+	mov rbp, rsp
+
+	; print the win message
+	mov rdi, win_message
+	mov rsi, 0
+	call printf
+	xor rax, rax ; clear the output
+
+	; reset the stack
+	mov rsp, rbp
+	pop rbp
+
+	ret
+
+_lose:
+	; configure the stack
+	push rbp
+	mov rbp, rsp
+
+	; print the lose mesage
+	mov rdi, lose_message
+	mov rsi, [random_number]
+	call printf
+	xor rax, rax
+
+	; reset stack
+	mov rsp, rbp
+	pop rbp
+
+	ret
